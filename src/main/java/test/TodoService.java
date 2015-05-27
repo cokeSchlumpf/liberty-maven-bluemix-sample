fc715b05c6411ab11a153665b92c17518e1ca1c7
Michael Wellner <michael.wellner@de.ibm.com>
2015-05-27 14:19:09 +0200

    moved persistence.xml to src/main/resources and some other small changes.

--- src/main/java/test/TodoService.java
+++ src/main/java/test/TodoService.java
@@ -10,21 +10,20 @@ import model.TodoItem;
 
 @Stateless
 @LocalBean
-@WebService(serviceName = "TodoService", name = "TodoService", //
-portName = "TodoBeanPort", targetNamespace = "http://xmlns.wellnr.com")
+@WebService(serviceName = "TodoService", name = "TodoService", portName = "TodoBeanPort", targetNamespace = "http://xmlns.wellnr.com")
 public class TodoService {
 
 	@PersistenceContext(unitName = "TodoApp")
 	private EntityManager em;
-	
+
 	public TodoItem add(String summary, String description) {
 		TodoItem item = new TodoItem();
 		item.setDescription(description);
 		item.setSummary(summary);
-		
+
 		em.persist(item);
-		
+
 		return item;
 	}
-	
+
 }
